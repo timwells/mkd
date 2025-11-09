8e9079724f9b888ed928f687cadfb0ae86d802ea
zx25 <timwells018@gmail.com>
2025-11-09 17:18:37 +0000

    Simple caching idea

--- src/stores/nt.ts
+++ src/stores/nt.ts
@@ -1,29 +1,41 @@
 import { defineStore } from 'pinia'
+import moment from 'moment'
+
 // const apiKey = import.meta.env.VITE_API_KEY
 const GFC = import.meta.env.VITE_GCF_URL
+const REQ_AGE_THRESHOLD = 60*5 // example threshold in milliseconds
 
 export const useNtStore = defineStore('nt', {
   state: () => ({
     data: [],
     loading: false,
     error: null as string | null,
+    nextReq: 0.0 as number
   }),
 
-  getters: {},
+  getters: {
+
+  },
 
   actions: {
     async getTrades(): Promise<void> {
-      this.loading = true
-      this.error = null
-      try {
-        const response = await fetch(`${GFC}/nt/trades`)
-        if (!response.ok) throw new Error('Failed to fetch items')
-        this.data = await response.json()
-        this.loading = false
-      } catch (err: any) {
-        this.error = err.message || 'Unknown error'
-      } finally {
-        this.loading = false
+      const now = moment().valueOf()
+      if (this.nextReq === 0.0 || now >= this.nextReq) {
+        this.nextReq = moment().add(REQ_AGE_THRESHOLD, 'minutes').valueOf()
+
+        this.loading = true
+        this.error = null
+
+        try {
+          const response = await fetch(`${GFC}/nt/trades`)
+          if (!response.ok) throw new Error('Failed to fetch items')
+          this.data = await response.json()
+          this.loading = false
+        } catch (err: any) {
+          this.error = err.message || 'Unknown error'
+        } finally {
+          this.loading = false
+        }
       }
     },
   },
