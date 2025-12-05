f718740dc2901e1093037d916e2e48f4bd3a16c2
zx25 <timwells018@gmail.com>
2025-12-05 15:58:04 +0000

    tweak

--- src/pages/views/MetalsPage.vue
+++ src/pages/views/MetalsPage.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <LightweightChartMtplMulti :tickers="['shiller-pe&mas=50,100,200']" type="line" />
+    <!--LightweightChartMtplMulti :tickers="['shiller-pe&mas=50,100,200']" type="line" /-->
+    <LightweightChartFTMulti :tickers="['UK10YG']" type="line" />
   </div>
 </template>
 RNWH
 <script setup>
-//import LightweightChartFTMulti from '@/components/lw-charts/LightweightChartFTMulti.vue'
+import LightweightChartFTMulti from '@/components/lw-charts/LightweightChartFTMulti.vue'
 //import LightweightChartMfMulti from '@/components/lw-charts/LightweightChartMfMulti.vue'
 import LightweightChartMtplMulti from '@/components/lw-charts/LightweightChartMtplMulti.vue'
 </script>
