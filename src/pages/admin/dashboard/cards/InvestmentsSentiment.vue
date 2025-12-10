59d0b9433e87c85e439a5976dc9c61ada157d5b5
zx25 <timwells018@gmail.com>
2025-12-10 22:24:32 +0000

    INvestment updates

--- src/pages/admin/dashboard/cards/InvestmentsSentiment.vue
+++ src/pages/admin/dashboard/cards/InvestmentsSentiment.vue
@@ -1,43 +1,72 @@
 <template>
   <VaCard square outlined class="rounded-xl">
-    <VaCardTitle>{{ investmentMarketTickers }}</VaCardTitle>
+    <VaCardTitle>{{ myMapInvestmentMarketTickers }}</VaCardTitle>
     <VaCardContent class="w-full" style="height: 600px">
-      <LightweightChartFTMulti :tickers="investmentMarketTickers" type="line" />
+      <LightweightChartFTMulti :tickers="myMapInvestmentMarketTickers" type="line" />
+    </VaCardContent>
+  </VaCard>
+  <VaCard square outlined class="rounded-xl">
+    <VaCardTitle>{{ moneyInvestmentMarketTickers }}</VaCardTitle>
+    <VaCardContent class="w-full" style="height: 600px">
+      <LightweightChartFTMulti :tickers="moneyInvestmentMarketTickers" type="line" />
+    </VaCardContent>
+  </VaCard>
+  <VaCard square outlined class="rounded-xl">
+    <VaCardTitle>{{ mgInvestmentMarketTickers }}</VaCardTitle>
+    <VaCardContent class="w-full" style="height: 600px">
+      <LightweightChartFTMulti :tickers="mgInvestmentMarketTickers" type="line" />
+    </VaCardContent>
+  </VaCard>
+  <VaCard square outlined class="rounded-xl">
+    <VaCardTitle>{{ avivaInvestmentMarketTickers }}</VaCardTitle>
+    <VaCardContent class="w-full" style="height: 600px">
+      <LightweightChartFTMulti :tickers="avivaInvestmentMarketTickers" type="line" />
     </VaCardContent>
   </VaCard>
 </template>
 
 <script lang="ts" setup>
 import LightweightChartFTMulti from '@/components/lw-charts/LightweightChartFTMulti.vue'
-const investmentMarketTickers = [
+const myMapInvestmentMarketTickers = [
   'GB00BFBFYB71:GBP',
   'GB00BFBFYJ57:GBP',
   'GB00BFBFYQ25:GBP',
   'GB00BFBFYX91:GBP',
   'GB00BFBFZ140:GBP',
   'GB00BN090M98:GBP',
-  //'GB00B18B9X76:GBP',
-  'IE00B3NS4D25:GBP',
 ]
 
-/*
-GB00BFBFYB71:GBP
-GB00BFBFYJ57:GBP
-GB00BFBFYQ25:GBP
-GB00BFBFYX91:GBP
-GB00BFBFZ140:GBP
-GB00BN090M98:GBP
-GB00B18B9X76:GBP
-IE00B3NS4D25:GBP
+const moneyInvestmentMarketTickers = [ 
+  "GB0007832602:GBP", 
+  "GB0007832487:GBP", 
+  "GB0007832826:GBP"
+]
 
-const FT_MY_MAPX_FUND_SYMBOLS = [
-    {name: "MyMap 3 Fund D Acc", symbol: "543012729", scale: 100.0},
-    {name: "MyMap 4 Fund D Acc", symbol: "542997798", scale: 100.0},
-    {name: "MyMap 5 Select ESG Fund D Acc", symbol: "604977373", scale: 100.0},
-    {name: "MyMap 6 Fund D Acc", symbol: "542990218", scale: 100.0},
-    {name: "MyMap 7 Select ESG Fund D Acc", symbol: "722345773", scale: 100.0},
-    {name: "Lindsell Train UK EqtyFund D Acc", symbol: "72625433", scale: 1.0}
-*/
+const mgInvestmentMarketTickers = [ 
+  //"GB0007832826:GBP", 
+  //"GB00B9GRH067:GBX", 
+  //"GB00B935C537:GBP", 
+  "GB00B6QQDK47:GBP", 
+  "GB0030927700:GBP",
+  "DIVI:LSE",
+  //"GB00B18B9X76:GBP",
+  //"IE00B3NS4D25:GBP"
+  //'GB00B18B9X76:GBP',
+  //'IE00B3NS4D25:GBP',
+]
+
+const avivaInvestmentMarketTickers = [ 
+  //"GB0007832826:GBP", 
+  "GB00B9GRH067:GBX", 
+  "GB00B935C537:GBP", 
+  //"GB00B6QQDK47:GBP", 
+  //"GB0030927700:GBP",
+  //"GB00B18B9X76:GBP",
+  //"IE00B3NS4D25:GBP"
+  //'GB00B18B9X76:GBP',
+  //'IE00B3NS4D25:GBP',
+]
+
 </script>
 
 <style scoped></style>
