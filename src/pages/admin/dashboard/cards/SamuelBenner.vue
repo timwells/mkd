d8236f82d0c5e585b729dcf832f056cc64d1879e
zx25 <timwells018@gmail.com>
2025-12-23 23:12:23 +0000

    Pyramid of Debt

--- src/pages/admin/dashboard/cards/SamuelBenner.vue
+++ src/pages/admin/dashboard/cards/SamuelBenner.vue
@@ -1,9 +1,16 @@
 <template>
-  <VaCard square outlined class="rounded-xl">
-    <VaCardContent>
-      <img src="../../../../assets/SamuelBenner2.png" width="100%" height="500" class="rounded-lg" />
-    </VaCardContent>
-  </VaCard>
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+    <VaCard square outlined class="rounded-xl">
+      <VaCardContent>
+        <img src="../../../../assets/SamuelBenner2.png" width="100%" class="rounded-lg" />
+      </VaCardContent>
+    </VaCard>
+    <VaCard square outlined class="rounded-xl">
+      <VaCardContent>
+        <img src="../../../../assets/pyramid.png" width="100%" class="rounded-lg" />
+      </VaCardContent>
+    </VaCard>
+  </div>
 </template>
 
 <script lang="ts" setup></script>
