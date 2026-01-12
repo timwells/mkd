f032e20fea3474ecf1383139888a559d46928218
zx25 <timwells018@gmail.com>
2026-01-12 19:53:43 +0000

    remove artemis.

--- src/pages/views/FundsPage.vue
+++ src/pages/views/FundsPage.vue
@@ -67,11 +67,6 @@ const fundinfo = [
     type: 'fund',
   },
   {
-    ticker: 'FJXZ1',
-    title: 'Artemis Global INCOME - GBP',
-    type: 'fund',
-  },
-  {
     ticker: 'FQXBQ',
     title: 'L&G Gbl Robotics & Automation Index - ACCUM GBP',
     type: 'fund',
